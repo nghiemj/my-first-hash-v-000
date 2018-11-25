c5d461a9a9291f6771365ad54217908769f0f607
James Nghiem <nghiem.j@hotmail.com>
2018-11-25 08:14:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,11 +6,6 @@ end
 def shipping_manifest
   
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
 end
 
 def retrieval
@@ -20,6 +15,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
+shipping_manifest[oil paintings]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
